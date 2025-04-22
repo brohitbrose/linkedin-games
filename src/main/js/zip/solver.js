8299ad5ec4575ee3555e07713b7e78c24ab500c6
brohitbrose <rohit.bose95@gmail.com>
2025-04-21 18:03:18 -0700

    Add unit tests for Zip solver. (#8)
    * Refactor Zip solver to publicly expose some methods.
    
    * Add unit tests, which caught a missing prune case.
    
    * Version bump

--- src/main/js/zip/solver.js
+++ src/main/js/zip/solver.js
@@ -29,7 +29,7 @@ function compressSequence(sequence) {
 }
 
 /** Class representing the grid state of a fresh Zip puzzle. */
-class ZipGrid {
+export class ZipGrid {
 
   /** The number of rows in this grid. */
   #m;
@@ -43,12 +43,19 @@ class ZipGrid {
   #foot;
   /** The current marked number that we have reached. */
   #current;
+  /** The explored path so far. */
+  #path;
+  /** The number of moves made so far. */
+  #visitedCells;
+  /** Bookkeeping stack of degree changes. */
+  #degreeModifications;
 
   /**
    * Stores the following for each cell:
    * 0. Whether it is visited
    * 1. The circled number that it includes if present, otherwise -1
-   * 2. The number of unvisited neighbors that are not wall-blocked
+   * 2. If this cell is visited, then the number of unvisited neighbors that are
+   *    not wall-blocked; otherwise, undefined behavior
    * 3. Whether its right neighbor is blocked by a wall
    * 4. Whether its down neighbor is blocked by a wall.
    */
@@ -61,6 +68,12 @@ class ZipGrid {
     this.#head = numberedCells[0];
     this.#foot = numberedCells[numberedCells.length - 1];
     this.#current = 1;
+
+    this.#path = new Array(this.#size).fill(-1);
+    this.#path[0] = this.#head;
+    this.#visitedCells = 1;
+    this.#degreeModifications = Array.from({length: this.#size - 1},
+      () => []);
 
     this.#cellStatuses = this.#constructCellStatuses(m, n, this.#size,
         numberedCells, downWalls, rightWalls);
@@ -84,12 +97,18 @@ class ZipGrid {
       const cellStatus = result[walledCell];
       cellStatus.addDownWall();
       cellStatus.decrementDegree();
+      if (walledCell < size - n) {
+        result[walledCell + n].decrementDegree();
+      }
     }
     // Right walls
     for (const walledCell of rightWalls) {
       const cellStatus = result[walledCell];
       cellStatus.addRightWall();
       cellStatus.decrementDegree();
+      if (walledCell % n !== n - 1) {
+        result[walledCell + 1].decrementDegree();
+      }
     }
     // Numbered cells
     for (let i = 0; i < numberedCells.length; i++) {
@@ -103,9 +122,8 @@ class ZipGrid {
    * visited to solve the puzzle.
    */
   solve() {
-    const result = [this.#head];
-    const degreeModifications = []; // stack
-    if (this.#backtrack(1, result, degreeModifications)) {
+    let result;
+    if (result = this.#backtrack()) {
       return result;
     } else {
       throw new Error("No solutions found");
@@ -116,51 +134,46 @@ class ZipGrid {
   // valid solution.
   // TODO: Implement this iteratively. In Queens, the number of stack frames is
   //  maximally the board dimension, which I've never seen exceed 11 in a real
-  //  puzzle. In Zip, it's the number of cells, which I've seen go up to 64.
-  #backtrack(depth, result, degreeModifications) {
-    if (depth === this.#size) {
-      return true;
+  //  puzzle. In Zip, it's the number of cells, which I've seen go up to 64, and
+  //  the use of the helper function compounds this number.
+  #backtrack() {
+    if (this.#visitedCells === this.#size) {
+      return [...this.#path];
     }
-    const lastMove = result[result.length - 1];
-    let shortCircuit = false;
-    if (this.#tryDirection(this.#canVisitUp(lastMove), lastMove - this.#n,
-        depth, result, degreeModifications, this.#visitUp)) {
-      return true;
+    const move = this.lastMove();
+    let result;
+    if (result = this.#tryDirection(this.canVisitUp(), move - this.#n, move,
+        this.#doVisitUp)) {
+      return result;
     }
-    if (this.#tryDirection(this.#canVisitDown(lastMove), lastMove + this.#n,
-        depth, result, degreeModifications, this.#visitDown)) {
-      return true;
+    if (result = this.#tryDirection(this.canVisitDown(), move + this.#n,
+        move, this.#doVisitDown)) {
+      return result;
     }
-    if (this.#tryDirection(this.#canVisitLeft(lastMove), lastMove - 1,
-        depth, result, degreeModifications, this.#visitLeft)) {
-      return true;
+    if (result = this.#tryDirection(this.canVisitLeft(), move - 1, move,
+        this.#doVisitLeft)) {
+      return result;
     }
-    if (this.#tryDirection(this.#canVisitRight(lastMove), lastMove + 1,
-        depth, result, degreeModifications, this.#visitRight)) {
-      return true;
+    if (result = this.#tryDirection(this.canVisitRight(), move + 1, move,
+        this.#doVisitRight)) {
+      return result;
     }
     return false;
   }
 
-  #tryDirection(canVisit, move, depth, path, degreeModifications, doVisit) {
+  #tryDirection(canVisit, move, lastMove, doVisit) {
     if (!canVisit) {
       return false;
     }
-    doVisit.call(this, move, path[path.length - 1], degreeModifications);
-    path.push(move);
-    const success = this.#backtrack(depth + 1, path, degreeModifications);
-    this.#unvisit(move, degreeModifications);
-    if (success) {
-      return true;
-    } else {
-      path.pop();
-      return false;
-    }
+    doVisit.call(this, move, lastMove);
+    const solution = this.#backtrack();
+    this.unvisit();
+    return solution;
   }
 
   /**
-   * Returns whether it is possible to visit the cell that is directly above the
-   * provided one. This method checks whether:
+   * Returns whether it is possible to extend the current path to move up. This
+   * method checks whether (let "src" be the last visited cell in #path):
    * - The "above cell" (hereafter "dst") exists in this grid
    * - dst has already been visited
    * - There is a wall between src and dst
@@ -169,47 +182,44 @@ class ZipGrid {
    *   only 1 unvisited cell attached to them, and similarly for the terminal
    *   cell except the check condition is 0 not 1.
    */
-  #canVisitUp(src) {
-    return this.#canVisitDirection(src,
-        s => s - this.#n,
+  canVisitUp() {
+    return this.#canVisitDirection(s => s - this.#n,
         (d, s) => d >= 0,
         (ds, ss) => ds.hasDownWall(),
         [this.#visitIsolatesDown, this.#visitIsolatesLeft,
             this.#visitIsolatesRight]);
   }
 
-  /** Same as #canVisitUp, but for the cell below the provided one. */
-  #canVisitDown(src) {
-    return this.#canVisitDirection(src,
-        s => s + this.#n,
+  /** Same as #canVisitUp, but for moving down. */
+  canVisitDown() {
+    return this.#canVisitDirection(s => s + this.#n,
         (d, s) => d < this.#size,
         (ds, ss) => ss.hasDownWall(),
         [this.#visitIsolatesUp, this.#visitIsolatesLeft,
             this.#visitIsolatesRight]);
   }
 
-  /** Same as #canVisitUp, but for the cell left of the provided one. */
-  #canVisitLeft(src) {
-    return this.#canVisitDirection(src,
-        s => s - 1,
+  /** Same as #canVisitUp, but for moving left. */
+  canVisitLeft() {
+    return this.#canVisitDirection(s => s - 1,
         (d, s) => s % this.#n !== 0,
         (ds, ss) => ds.hasRightWall(),
         [this.#visitIsolatesUp, this.#visitIsolatesDown,
             this.#visitIsolatesRight]);
   }
 
-  /** Same as #canVisitUp, but for the cell right of the provided one. */
-  #canVisitRight(src) {
-    return this.#canVisitDirection(src,
-        s => s + 1,
+  /** Same as #canVisitUp, but for moving right. */
+  canVisitRight() {
+    return this.#canVisitDirection(s => s + 1,
         (d, s) => s % this.#n !== this.#n - 1,
         (ds, ss) => ss.hasRightWall(),
         [this.#visitIsolatesUp, this.#visitIsolatesDown,
             this.#visitIsolatesLeft]);
   }
 
-  #canVisitDirection(src, computeDst, checkDstSrcBounds, checkDstSrcWall,
+  #canVisitDirection(computeDst, checkDstSrcBounds, checkDstSrcWall,
       willIsolateFns) {
+    const src = this.lastMove();
     const dst = computeDst.call(this, src);
     const dstStatus = this.#cellStatuses[dst];
     const srcStatus = this.#cellStatuses[src];
@@ -254,31 +264,63 @@ class ZipGrid {
         || (cell !== this.#foot && degree === 2);
   }
 
-  #visitUp(dst, src, degreeModifications) {
-    this.#visitDirection(dst, src, degreeModifications,
-        [this.#visitImpactsDownDegree, this.#visitImpactsLeftDegree,
-            this.#visitImpactsRightDegree]);
+  visitUp() {
+    if (this.canVisitUp()) {
+      const lastMove = this.lastMove();
+      this.#doVisitUp(lastMove - this.#n, lastMove);
+      return true;
+    }
+    return false;
   }
 
-  #visitDown(dst, src, degreeModifications) {
-    this.#visitDirection(dst, src, degreeModifications,
-        [this.#visitImpactsUpDegree, this.#visitImpactsLeftDegree,
-            this.#visitImpactsRightDegree]);
+  #doVisitUp(dst, src) {
+    this.#visitDirection(dst, src, [this.#visitImpactsDownDegree,
+        this.#visitImpactsLeftDegree, this.#visitImpactsRightDegree]);
   }
 
-  #visitLeft(dst, src, degreeModifications) {
-    this.#visitDirection(dst, src, degreeModifications,
-        [this.#visitImpactsUpDegree, this.#visitImpactsDownDegree,
-            this.#visitImpactsRightDegree]);
+  visitDown() {
+    if (this.canVisitDown()) {
+      const lastMove = this.lastMove();
+      this.#doVisitDown(lastMove + this.#n, lastMove);
+      return true;
+    }
+    return false;
   }
 
-  #visitRight(dst, src, degreeModifications) {
-    this.#visitDirection(dst, src, degreeModifications,
-        [this.#visitImpactsUpDegree, this.#visitImpactsDownDegree,
-          this.#visitImpactsLeftDegree]);
+  #doVisitDown(dst, src) {
+    this.#visitDirection(dst, src, [this.#visitImpactsUpDegree,
+        this.#visitImpactsLeftDegree, this.#visitImpactsRightDegree]);
   }
 
-  #visitDirection(dst, src, degreeModifications, impactFns) {
+  visitLeft() {
+    if (this.canVisitLeft()) {
+      const lastMove = this.lastMove();
+      this.#doVisitLeft(lastMove - 1, lastMove);
+      return true;
+    }
+    return false;
+  }
+
+  #doVisitLeft(dst, src) {
+    this.#visitDirection(dst, src, [this.#visitImpactsUpDegree,
+        this.#visitImpactsDownDegree, this.#visitImpactsRightDegree]);
+  }
+
+  visitRight() {
+    if (this.canVisitRight()) {
+      const lastMove = this.lastMove();
+      this.#doVisitRight(lastMove + 1, lastMove);
+      return true;
+    }
+    return false;
+  }
+
+  #doVisitRight(dst, src) {
+    this.#visitDirection(dst, src,[this.#visitImpactsUpDegree,
+        this.#visitImpactsDownDegree, this.#visitImpactsLeftDegree]);
+  }
+
+  #visitDirection(dst, src, impactFns) {
     const dstStatus = this.#cellStatuses[dst];
     // Mark cell as visited.
     dstStatus.setVisited(true);
@@ -287,8 +329,8 @@ class ZipGrid {
     if (dstContent > 0) {
       this.#current = dstContent; 
     }
-    // Check if degrees need to be modified.
-    const newModifications = [];
+    // Modify degrees as needed.
+    const newModifications = this.#degreeModifications[this.#visitedCells - 1];
     for (const impactFn of impactFns) {
       const cell = impactFn.call(this, src);
       if (cell >= 0) {
@@ -296,23 +338,31 @@ class ZipGrid {
         newModifications.push(cell);
       }
     }
-    degreeModifications.push(newModifications);
+    // Update path, visitedCells
+    this.#path[this.#visitedCells++] = dst;
   }
 
   #visitImpactsUpDegree(src) {
-    const up = src - this.#n;
-    const upStatus = this.#cellStatuses[up];
-    return up >= 0 && !upStatus.isVisited() && !upStatus.hasDownWall()
-        ? up : -1;
+    if (src >= this.#n) {
+      const up = src - this.#n;
+      const upStatus = this.#cellStatuses[up];
+      if (!upStatus.isVisited() && !upStatus.hasDownWall()) {
+        return up;
+      }
+    }
+    return -1;
   }
 
   #visitImpactsDownDegree(src) {
-    const down = src + this.#n;
-    const downStatus = this.#cellStatuses[down];
-    const srcStatus = this.#cellStatuses[src];
-    return down < this.#size
-          && !downStatus.isVisited() && !srcStatus.hasDownWall()
-        ? down : -1;
+    if (src < this.#size - this.#n) {
+      const down = src + this.#n;
+      const downStatus = this.#cellStatuses[down];
+      const srcStatus = this.#cellStatuses[src];
+      if (!downStatus.isVisited() && !srcStatus.hasDownWall()) {
+        return down;
+      }
+    }
+    return -1;
   }
 
   #visitImpactsLeftDegree(src) {
@@ -338,20 +388,31 @@ class ZipGrid {
     return -1;
   }
 
-  #unvisit(move, degreeModifications) {
+  unvisit() {
+    if (this.#visitedCells == 1) {
+      return false;
+    }
+    const lastMove = this.lastMove();
+    // Remove last move from path and update visitedCells value.
+    this.#path[(this.#visitedCells--) - 1] = -1;
     // Possibly revert degree modifications.
-    const modifications = degreeModifications.pop();
-    for (const cell of modifications) {
-      this.#cellStatuses[cell].incrementDegree();
+    const modifications = this.#degreeModifications[this.#visitedCells - 1];
+    while (modifications.length !== 0) {
+      this.#cellStatuses[modifications.pop()].incrementDegree();
     }
     // Possibly revert last-found circled number, 
-    const moveStatus = this.#cellStatuses[move];
+    const moveStatus = this.#cellStatuses[lastMove];
     const moveContent = moveStatus.getContent();
     if (moveContent > 0) {
       this.#current = moveContent - 1;
     }
     // Mark cell as unvisited.
     moveStatus.setVisited(false);
+    return true;
+  }
+
+  lastMove() {
+    return this.#path[this.#visitedCells - 1];
   }
 
 }
---
+++ src/test/js/zip/solver.test.js
@@ -0,0 +1,80 @@
+import { ZipGrid, solver } from 'zip/solver.js';
+
+const gridSeed = [
+  6,
+  6,
+  [25, 7, 1, 34, 10, 28],
+  [7, 8, 21, 22],
+  [12, 16, 18, 22],
+];
+
+test('Visit-type methods behave as expected', () => {
+  const grid = new ZipGrid(gridSeed[0], gridSeed[1], gridSeed[2], gridSeed[3],
+      gridSeed[4]);
+  // Move 1: no issues in moving up.
+  expect(grid.visitUp()).toBe(true);
+
+  // Move 2a: can't visit left due to wall.
+  expect(grid.visitLeft()).toBe(false);
+  // Move 2b: can't visit right since it will isolate cell 13 (early prune).
+  expect(grid.visitRight()).toBe(false);
+  // Move 2b: can't visit down since it's already in path.
+  expect(grid.visitDown()).toBe(false);
+  // Move 2c: no issues in moving up again.
+  expect(grid.visitUp()).toBe(true);
+
+  // Move 3a: can't move up due to wall.
+  expect(grid.visitUp()).toBe(false);
+  // Move 3b: can't move down since it's already in path.
+  expect(grid.visitDown()).toBe(false);
+  // Move 3c: can't move left due to wall.
+  expect(grid.visitLeft()).toBe(false);
+  // Move 3d: no issues in moving right.
+  expect(grid.visitRight()).toBe(true);
+
+  // Move as far right as we can.
+  expect(grid.visitRight()).toBe(true);
+  expect(grid.visitRight()).toBe(true);
+  // Can't move right again due to wall.
+  expect(grid.visitRight()).toBe(false);
+  // Can't move up since it's too early to touch 5.
+  expect(grid.visitUp()).toBe(false);
+  // Move down.
+  expect(grid.visitDown()).toBe(true);
+  // Can't move down again due to wall (now we've tested all wall types).
+  expect(grid.visitDown()).toBe(false);
+  // Undo this detour.
+  expect(grid.unvisit()).toBe(true);
+  expect(grid.unvisit()).toBe(true);
+
+  // Make our way to just right of the 3.
+  expect(grid.visitUp()).toBe(true);
+  expect(grid.visitUp()).toBe(true);
+  expect(grid.visitLeft()).toBe(true);
+  // Can't visit 3 just yet.
+  expect(grid.visitLeft()).toBe(false);
+  // But we can visit the 2 below it.
+  expect(grid.visitDown()).toBe(true);
+  expect(grid.visitLeft()).toBe(true);
+  // And now we can visit the 3.
+  expect(grid.visitUp()).toBe(true);
+});
+
+test('ZipGrid.solve() generates the correct solution', () => {
+  const grid = new ZipGrid(gridSeed[0], gridSeed[1], gridSeed[2], gridSeed[3],
+      gridSeed[4]);
+  const actual = grid.solve();
+  const expected = [
+      25, 19, 13, 14, 15,  9,
+      3,  2,  8, 7,  1,  0, 
+      6, 12, 18, 24, 30, 31,
+      32, 33, 34, 35, 29, 23,
+      17, 11,  5, 4, 10, 16,
+      22, 21, 20, 26, 27, 28
+  ];
+  expect(actual.length).toBe(36);
+  expect(expected.length).toBe(36);
+  for (let i = 0; i < 36; i++) {
+    expect(actual[i]).toBe(expected[i]);
+  }
+});
