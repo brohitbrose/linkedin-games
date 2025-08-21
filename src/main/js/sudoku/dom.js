1c1f4910b42e4b5593a6d661df85b1e2133ff889
Rohit Bose <rohit.bose95@gmail.com>
2025-08-21 00:29:48 -0700

    Spike sudoku solver.

---
+++ src/main/js/sudoku/dom.js
@@ -0,0 +1,48 @@
+// import { doOneClick, getGridDiv } from '../util.js';
+
+// export function autoSolve() {
+//   const prioritizedApis = [new SudokuDomApiV0()];
+//   for (let i = 0; i < prioritizedApis.length; ) {
+//     const api = prioritizedApis[i];
+//     try {
+//       api.autoSolve();
+//       return;
+//     } catch (e) {
+//       console.error(e);
+//       if (++i !== prioritizedApis.length) {
+//         console.info('Will reattempt autoSolve() via a prior API');
+//       } else {
+//         console.error('All APIs exhausted');
+//       }
+//     }
+//   }
+// }
+
+// class SudokuDomApi {
+
+//   autoSolve() {
+//     const gridDiv = this.getSudokuGridDiv();
+//     const notesDiv = this.getNotesDiv();
+//     const numberDivs = this.getNumbers
+//     const clickS
+//   }
+
+//   disableNotes(notesDiv) {
+
+//   }
+
+// }
+
+// class SudokuDomApiV0 {
+
+//   getNotesIsOn(notesDiv) {
+
+//   }
+
+//   cellDivIsLocked(cellDiv) {
+//     return cellDiv.classList.contains('sudoku-cell-prefilled');
+//   }
+
+//   ce
+
+// }
